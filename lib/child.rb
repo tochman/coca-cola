352480e222ab92092eb33950c3f32a538f3090c8
tochman <thomas@craftacademy.se>
2017-02-09 10:52:42 +0100

    adds more tests

--- lib/child.rb
+++ lib/child.rb
@@ -1,3 +1,20 @@
 class Child
 
+  attr_accessor :has_coke
+
+  def initialize
+    @has_coke = false
+  end
+
+  def fetch_coke(resource)
+    resource.has_coke = false
+    @has_coke = true
+  end
+
+  def has_coke?
+    @has_coke
+  end
+
+
+
 end
--- spec/child_spec.rb
+++ spec/child_spec.rb
@@ -3,8 +3,23 @@ require './lib/child'
 
 describe Child do
 
+  let(:my_fridge) { double('Fridge', has_coke: true) }
+
+  before do
+    allow(my_fridge).to receive(:has_coke=).and_return(false)
+  end
+
   it 'should be an instance of Child' do
-    expect(Child.new).to be_an_instance_of Child
+    expect(subject).to be_an_instance_of Child
+  end
+
+  it 'has no coke when created' do
+    expect(subject.has_coke).to eq false
+  end
+
+  it 'should be able to give me a can of Coke' do
+    subject.fetch_coke(my_fridge)
+    expect(subject).to have_coke
   end
 
 end
